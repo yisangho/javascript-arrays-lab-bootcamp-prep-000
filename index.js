d2f7fb5bd02ba288e14ae658f003b78a6ff27214
yisangho <yisangho717@gmail.com>
2018-08-24 05:41:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function destructivelyRemoveLastKitten(name) {
 }
 
 function destructivelyRemoveFirstKitten(name) {
-  kittens = kittens.slice(-1);
+  kittens = kittens.shift();
   return kittens
 }
 
